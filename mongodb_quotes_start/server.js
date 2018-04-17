d323a42381123a1c70968d3f31b9f7eab9d4a218
Sarah Campbell <scampbell.home@btinternet.com>
2018-04-17 23:31:56 +0100

    Added a get route to our node express server

--- mongodb_quotes_start/server.js
+++ mongodb_quotes_start/server.js
@@ -13,9 +13,39 @@ MongoClient.connect('mongodb://localhost:27017', function(err, client) {
     console.log(err);
     return;
   }
-  const db = client.db("recipes");
+  const db = client.db("house");
 
   console.log('Connected to database');
+
+  // CREATE [POST] route...
+  server.post('/api/rooms', function(req, res){
+    const roomsCollection = db.collection('rooms');
+    const roomToSave = req.body;
+    roomsCollection.save(roomToSave, function(err, result){
+      if (err) {
+        console.log(err);
+        res.status(500)
+        res.send();
+      }
+      console.log("Room saved as a document to 'rooms' collection in 'house' database");
+      res.status(201);
+      res.json(result.ops[0]);
+    });
+  });
+
+  // READ [GET] route...
+  server.get('/api/rooms', function(req, res) {
+    const roomsCollection = db.collection('rooms');
+    roomsCollection.find().toArray(function(err, results){
+      if(err) {
+        console.log(err);
+        res.status(500)
+        res.send();
+      }
+      console.log("Got all documents in 'rooms' collection in 'house' database");
+      res.json(results);
+    });
+  });
 
   server.listen(3000, function(){
     console.log("Listening on port 3000");
